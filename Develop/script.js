9c8e525c7c1c8047ecdef3badb7512aac4fa2e64
Maya Tillman <mayatillman17@gmail.com>
2022-01-09 23:08:22 -0600

    generate

---
+++ Develop/script.js
@@ -0,0 +1,115 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+//Arrary//
+let sc = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+let uc = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let lc = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+//Check for Password Length//
+function generatePassword() {
+  let bigBoipassword = "";
+  let passwordLength = window.prompt(
+    "Please enter your desired password length. Note: The length must be at least 8 characters and no more than 128 characters."
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("The password length you've enetered is invalid. Try again.");
+  } else {
+    let includeSpecial = confirm(
+      "Do you want special characters in your password?"
+    );
+    let includeUpper = confirm(
+      "Do you want uppercase letters in your password?"
+    );
+    let includeLower = confirm(
+      "Do you want lowercase letters in your password?"
+    );
+    let includeNumbers = confirm("Do you want numbers in your password?");
+
+    //Loop through array which adds user choice to overall password//
+    if (includeSpecial === true) {
+      bigBoipassword = bigBoipassword.concat(sc);
+    }
+
+    if (includeUpper === true) {
+      bigBoipassword = bigBoipassword.concat(uc);
+    }
+
+    if (includeLower === true) {
+      bigBoipassword = bigBoipassword.concat(lc);
+    }
+
+    if (includeNumbers === true) {
+      bigBoipassword = bigBoipassword.concat(numbers);
+    }
+    console.log(bigBoipassword);
+  }
+  for (let i = 0; i < passwordLength.length; i++) {
+    return bigBoipassword[Math.floor(Math.random() * bigBoipassword.length)];
+  }
+}
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# hw3b
+# 03 JavaScript: Password Generator
+
+## Description
+
+The Password Generator is a tool to have a password generated just for you! It takes into consideration personal preferences in regards to the password (e.g. special characters, lowercase letters, uppercase letters, numbers, etc). The Password Generator is very user friendly in terms of accessibility, and it contains easy navigation to optimize the user's experience.
+
+## Screenshot
+
+![The Password Generator includes a user button to initiate the generator where a randomized password will appear one each time. A window screen is available so that the user can see the password that was created for him or her.](./assets/images/03-javascript-homework-demo.png)
+
+## Links to Application
+
+- Here is the repo : [mayaatil/repo](https://github.com/mayaatil/hw3)
+- Here is the pages : [mayaatil/pages](https://mayaatil.github.io/hw3/)
